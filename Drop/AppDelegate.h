c9c36551701195414e2f5430a723356c0fd0b36c
Kyle Dillon <kjdillon3@gmail.com>
2014-12-12 17:04:10 -0800

    Initial Commit

---
+++ Drop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Drop
+//
+//  Created by Kyle Dillon on 12/12/14.
+//  Copyright (c) 2014 KJDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Drop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Drop
+//
+//  Created by Kyle Dillon on 12/12/14.
+//  Copyright (c) 2014 KJDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
